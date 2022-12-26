c8b977c1f601a78b7536a0c366fffa60cfef50e0
DarkDev <mda.business.on@gmail.com>
2022-12-26 22:24:53 +0500

    Completed Form And Menus

--- script.js
+++ script.js
@@ -18,3 +18,13 @@ function HandleNavClick()
         navButton.classList.add('fa-bars');
     }
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
