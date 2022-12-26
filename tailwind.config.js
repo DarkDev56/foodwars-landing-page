40d813638fffba4893afaed4976a237c99e48005
DarkDev <mda.business.on@gmail.com>
2022-12-26 18:46:18 +0500

    Completed Markup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "roboto": "'Roboto', sans-serif",
+        "pt-sans": "'PT Sans', sans-serif",
+        "bebas": "'Bebas Neue', cursive"
+      }
+    },
+  },
+  plugins: [],
+}
