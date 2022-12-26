b1f6096c4b59f75b84ec1e3c54888bc938090ce6
DarkDev <mda.business.on@gmail.com>
2022-12-26 20:42:20 +0500

    Completed Hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
       fontFamily:{
         "roboto": "'Roboto', sans-serif",
         "pt-sans": "'PT Sans', sans-serif",
-        "bebas": "'Bebas Neue', cursive"
+        "viga": "'Viga', cursive"
       }
     },
   },
